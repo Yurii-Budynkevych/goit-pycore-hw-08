60adbb8eed38a67e18f5040419700bf20902d662
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2025-07-22 01:14:20 +0200

    fix

--- classes.py
+++ classes.py
@@ -11,22 +11,22 @@ class Field:
     def __init__(self, value):
         self.value = value
         
-    def get_value (self):
+    def get_value(self):
         return str(self.value)
 
 class Name(Field):
     def __init__(self, name):
-        name = name.lower().capitalize() 
-        super().__init__(name)    
+        name = name.lower().capitalize()
+        super().__init__(name)
 
 class Phone(Field):
-    __pattern = r"^\+?\d{10,}$"
+    __pattern = r"^\+?\d{10,15}$"
 
     def __init__(self, phone):
         if not phone:
-                raise PhoneError("Plese enter phone")
+            raise PhoneError("Please enter phone")
         elif not re.match(self.__pattern, phone):
-                raise PhoneError() 
+            raise PhoneError(f"Invalid phone format: {phone}")
         super().__init__(phone)
 
 class Birthday(Field):
@@ -34,44 +34,57 @@ class Birthday(Field):
         try:
             self.date = datetime.strptime(value, '%d.%m.%Y')
         except ValueError:
-            raise ValueError("Invalid date format. Use DD.MM.YYYY")    
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
 
     def __repr__(self):
-        return f"Object Birthday. birthday: {self.date}"
+        return f"{self.date.strftime('%d.%m.%Y')}"
 
+    def upcoming_this_year(self):
+        today = datetime.today()
+        upcoming = self.date.replace(year=today.year)
+        if upcoming < today:
+            upcoming = upcoming.replace(year=today.year + 1)
+        return upcoming
 
 class Record:
     def __init__(self, name, phone=None, birthday=None):
         self.name = Name(name)
-        self.phones = [Phone(phone)]
-        self.birthday = birthday
-  
-    def add_phone (self, phone):
-        new_phone = Phone(phone) 
+        self.phones = []
+        if phone:
+            self.add_phone(phone)
+        self.birthday = None
+        if birthday:
+            self.add_birthday(birthday)
+
+    def add_phone(self, phone):
+        new_phone = Phone(phone)
         self.phones.append(new_phone)
 
-    def edit_phone (self, phone, edited_phone):
-        for el in self.phones:
+    def edit_phone(self, phone, edited_phone):
+        edited = Phone(edited_phone)
+        for i, el in enumerate(self.phones):
             if el.value == phone:
-                el.value = edited_phone
+                self.phones[i] = edited
+                return
+        raise PhoneError("Old phone number not found.")
 
-    def find_phone (self, phone):
+    def find_phone(self, phone):
         for el in self.phones:
             if el.value == phone:
                 return phone
         return 'not found'
-    
-    def get_phones (self):
+
+    def get_phones(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
-    
-    def add_birthday (self, birthday):
-        new_birthday = Birthday(birthday) 
+
+    def add_birthday(self, birthday):
+        new_birthday = Birthday(birthday)
         self.birthday = new_birthday
 
-    def get_birthday (self):
+    def get_birthday(self):
         return f"Contact name: {self.name.value}, birthday: {self.birthday}"
-    
-    def __repr__ (self):
+
+    def __repr__(self):
         return f"Object Record. phones: {'; '.join(p.value for p in self.phones)}, birthday: {self.birthday}"
 
 class AddressBook(UserDict):
@@ -80,14 +93,16 @@ class AddressBook(UserDict):
 
     def find(self, record_name):
         return self.data.get(record_name.lower().capitalize())
-    
+
     def show_records_birthdays(self):
-        birthday_list = []
-        for record_name, record in self.data.items():
+        today = datetime.today()
+        result = []
+        for record in self.data.values():
             if record.birthday:
-                birthday_list.append((record_name, record.get_birthday()))
-        return birthday_list
+                upcoming = record.birthday.upcoming_this_year()
+                result.append(f"{record.name.value}: {upcoming.strftime('%d.%m.%Y')}")
+        return "\n".join(result)
 
     def delete(self, record_name):
-        self.data.pop(record_name) 
+        self.data.pop(record_name, None)
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from classes import AddressBook, Record
 import pickle
 
 def parse_input(user_input):
-    cmd, *args = user_input.split()
+    cmd, *args = user_input.strip().split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
@@ -15,91 +15,101 @@ def load_data(filename="addressbook.pkl"):
         with open(filename, "rb") as f:
             return pickle.load(f)
     except FileNotFoundError:
-        return AddressBook()  # Повернення нової адресної книги, якщо файл не знайдено
+        return AddressBook()
 
 def main():
     book = load_data()
     print("Welcome to the assistant bot!")
     while True:
-        user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
+        try:
+            user_input = input("Enter a command: ").strip()
+            if not user_input:
+                continue
 
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            save_data(book)
-            break
+            command, *args = parse_input(user_input)
 
-        elif command == "hello":
-            print("How can I help you?")
+            if command in ["close", "exit"]:
+                print("Good bye!")
+                save_data(book)
+                break
 
-        elif command == "add":
-            if not args:
-                raise Exception('please enter name')
-            elif args and book.find(args[0]):
-                record = book.find(args[0])
-                try:
-                    record.add_phone(args[1])
-                except Exception as e:
-                    print("Error Occured, plese make sure to pass phone number as second argument: ", e)        
-            else:    
-                book.add_record(Record(*args))
+            elif command == "hello":
+                print("How can I help you?")
 
-        elif command == "change":
-            if not args:
-                raise Exception('please enter name')
-            elif args and book.find(args[0]):
-                record = book.find(args[0])
-                try:
-                    record.edit_phone(args[1], args[2])
-                except Exception as e:
-                    print("Error Occured, plese make sure to pass phone number to edit and new value: ", e)        
-            else:    
-                print(f"Record with name {args[0]} not found")
+            elif command == "add":
+                if not args:
+                    raise Exception("please enter name")
+                elif args and book.find(args[0]):
+                    record = book.find(args[0])
+                    try:
+                        record.add_phone(args[1])
+                    except Exception as e:
+                        print("Error: ", e)
+                else:
+                    try:
+                        book.add_record(Record(*args))
+                    except Exception as e:
+                        print("Error: ", e)
 
-        elif command == "phone":
-            if not args:
-                raise Exception('please enter name')
-            elif args and book.find(args[0]):
-                record = book.find(args[0])
-                try:
+            elif command == "change":
+                if not args:
+                    raise Exception("please enter name")
+                elif args and book.find(args[0]):
+                    record = book.find(args[0])
+                    try:
+                        record.edit_phone(args[1], args[2])
+                    except Exception as e:
+                        print("Error: ", e)
+                else:
+                    print(f"Record with name {args[0]} not found")
+
+            elif command == "phone":
+                if not args:
+                    raise Exception("please enter name")
+                elif args and book.find(args[0]):
+                    record = book.find(args[0])
+                    try:
+                        print(record.get_phones())
+                    except Exception as e:
+                        print("Error: ", e)
+                else:
+                    print(f"Record with name {args[0]} not found")
+
+            elif command == "all":
+                for record in book.values():
                     print(record.get_phones())
-                except Exception as e:
-                    print("Error Occured: ", e)        
-            else:    
-                print(f"Record with name {args[0]} not found")
 
-        elif command == "all":
-            print(book)
+            elif command == "add-birthday":
+                if not args:
+                    raise Exception("please enter name")
+                elif args and book.find(args[0]):
+                    record = book.find(args[0])
+                    try:
+                        record.add_birthday(args[1])
+                    except Exception as e:
+                        print("Error: ", e)
+                else:
+                    print(f"Record with name {args[0]} not found")
 
-        elif command == "add-birthday":
-            if not args:
-                raise Exception('please enter name')
-            elif args and book.find(args[0]):
-                record = book.find(args[0])
-                try:
-                    record.add_birthday(args[1])
-                except Exception as e:
-                    print("Error Occured, make sure passing the date: ", e)        
-            else:    
-                print(f"Record with name {args[0]} not found")
+            elif command == "show-birthday":
+                if not args:
+                    raise Exception("please enter name")
+                elif args and book.find(args[0]):
+                    record = book.find(args[0])
+                    try:
+                        print(record.get_birthday())
+                    except Exception as e:
+                        print("Error: ", e)
+                else:
+                    print(f"Record with name {args[0]} not found")
 
-        elif command == "show-birthday":
-            if not args:
-                raise Exception('please enter name')
-            elif args and book.find(args[0]):
-                record = book.find(args[0])
-                try:
-                    print(record.get_birthday())
-                except Exception as e:
-                    print("Error Occured, make sure passing the date: ", e)        
-            else:    
-                print(f"Record with name {args[0]} not found")
+            elif command == "birthdays":
+                print(book.show_records_birthdays())
 
-        elif command == "birthdays":
-            print(book.show_records_birthdays())
+            else:
+                print("Invalid command.")
+        except Exception as e:
+            print("Error:", e)
 
-        else:
-            print("Invalid command.")
-
-if __name__ == '__main__':     
+if __name__ == '__main__':
     main()
